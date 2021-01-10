ff0d9af90ef30bd75ef490b429098d08bede7813
fandujar <filipeandujar@gmail.com>
2021-01-10 04:02:35 -0300

    add event emitter test

--- baymax/src/app.module.ts
+++ baymax/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import {EventEmitterModule} from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventEmitterModule } from '@nestjs/event-emitter';
-
+ 
 
 import {SlackModule} from './slack/slack.module'
 
@@ -11,7 +11,7 @@ import {SlackModule} from './slack/slack.module'
     EventEmitterModule.forRoot({
       wildcard: true,
       newListener: true,
-      removeEventListener: true,
+      removeListener: true,
     }),
     SlackModule
   ],
--- baymax/src/slack/slack.module.ts
+++ baymax/src/slack/slack.module.ts
@@ -1,4 +1,22 @@
-import {Module} from '@nestjs/common'
+import { Module, OnModuleInit } from '@nestjs/common';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+
 
 @Module({})
-export class SlackModule {}
+export class SlackModule implements OnModuleInit {
+    constructor(private eventEmitter: EventEmitter2) {}
+
+    onModuleInit(){
+        this.eventEmitter.emit(
+            'baymax.slack.initialize',
+            {
+                message: 'Slack module initialized'
+            },
+        );
+    }
+    
+    @OnEvent('baymax.slack.initialize', { async: true })
+    handleSlackModuleInitialize(payload: Object) {
+      console.log(payload)
+    }
+}
